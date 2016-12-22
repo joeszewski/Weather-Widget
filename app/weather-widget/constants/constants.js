269a87ccb3ebfc77c9ce8f05becbb92191aa7543
JoeSzewski <Joe@Joes-MacBook-Pro-2.local>
2016-12-22 17:52:01 -0600

    created a constants file which contains the forecast.io API key, and the root of the API call, and import both of these values into the weather.service.ts file

---
+++ app/weather-widget/constants/constants.js
@@ -0,0 +1,4 @@
+"use strict";
+exports.FORECAST_KEY = "7df075f8a4c77e33be9b715f3a09ea77";
+exports.FORECAST_ROOT = "https://api.darksky.net/forecast/";
+//# sourceMappingURL=constants.js.map
---
+++ app/weather-widget/constants/constants.ts
@@ -0,0 +1,2 @@
+export const FORECAST_KEY = "7df075f8a4c77e33be9b715f3a09ea77";
+export const FORECAST_ROOT = "https://api.darksky.net/forecast/";
