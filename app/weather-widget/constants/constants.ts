af16768b70505aba25d01edf28e211e7716e72f6
JoeSzewski <Joe@Joes-MacBook-Pro-2.local>
2016-12-30 16:20:07 -0600

    added google API key and root to contants file. Also added the constants folder to the gitignore file

--- app/weather-widget/constants/constants.ts
+++ app/weather-widget/constants/constants.ts
@@ -1,2 +1,4 @@
 export const FORECAST_KEY = "7df075f8a4c77e33be9b715f3a09ea77";
 export const FORECAST_ROOT = "https://api.darksky.net/forecast/";
+export const GOOGLE_KEY = "AIzaSyBQElFc469Y8iswt-g2rPSs9BN0n7V2zGg";
+export const GOOGLE_ROOT = "https://maps.googleapis.com/maps/api/geocode/json";
--- app/weather-widget/service/weather.service.ts
+++ app/weather-widget/service/weather.service.ts
@@ -4,7 +4,7 @@ import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/operator/map';
 import 'rxjs/add/operator/catch';
 
-import { FORECAST_KEY, FORECAST_ROOT } from '../constants/constants';
+import { FORECAST_KEY, FORECAST_ROOT, GOOGLE_KEY, GOOGLE_ROOT } from '../constants/constants';
 
 @Injectable()
 export class WeatherService {
